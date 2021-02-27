9ce3f2f4dfbafb764239ca2306e42014215c11f9
lrhtony <tony07017@163.com>
2021-02-28 03:08:23 +0800

    Signed-off-by: lrhtony <tony07017@163.com>

--- unlock-music/service-worker.js
+++ unlock-music/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6854feef646b0f61d7911d9c92d80120.js"
+  "precache-manifest.4089eb90a20a50fc021e5f61ce09db71.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
